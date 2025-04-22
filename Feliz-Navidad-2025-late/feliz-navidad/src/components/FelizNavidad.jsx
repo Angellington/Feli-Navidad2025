3e420e9a6670d42e3b6db8919a59e52f1706cad7
Seu Nome de Usuário <superetro85@gmail.com>
2025-04-22 15:14:19 -0500

    imagens randomicas

--- Feliz-Navidad-2025-late/feliz-navidad/src/components/FelizNavidad.jsx
+++ Feliz-Navidad-2025-late/feliz-navidad/src/components/FelizNavidad.jsx
@@ -1,7 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+import '../styles/css/feliznavidad.css'
+
+// import das imagens em uma lista
+import Img1 from '../assets/backrandom/image1.png'
+import Img2 from '../assets/backrandom/image2.png'
+import Img3 from '../assets/backrandom/image3.png'
+import Img4 from '../assets/backrandom/image4.png'
+import Img5 from '../assets/backrandom/image5.png'
+import Img6 from '../assets/backrandom/image6.png'
+import Img7 from '../assets/backrandom/image7.png'
+import Img8 from '../assets/backrandom/image8.png'
+
+
 const FelizNavidad = () => {
+    // Lista das imagens
+    const images_list = [ Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8 ];
+
+    let rand = Math.floor(Math.random() * images_list.length);
+
+
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -13,7 +32,15 @@ const FelizNavidad = () => {
 
   return (
     <div>
-        <h2>Feliz Navidad 2025</h2>
+        <article className="container-image">
+            <img src={images_list[rand]} alt="Imagem 1" />
+        </article>
+
+        <button onClick={handleClick}>VOLTAR</button>
+
+
+
+        {/* <h2>Feliz Navidad 2025</h2>
         <button onClick={handleClick}>VOLTAR</button>
         <article>
             <h3>Imagens Recebidas</h3>
@@ -24,7 +51,7 @@ const FelizNavidad = () => {
                 </div>
             ))}
             
-        </article>
+        </article> */}
     </div>
   )
 }
